3ed57aa61f52462e3a807084364e3b6457fd4c7d
Kate Fang <katefang33@gmail.com>
2020-10-31 14:46:58 -0400

    set up login register forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Log In to Your Account
+      </p>
+      <form
+        className='form'
+        action='create-profile.html'
+        onSubmit={handleSubmit}
+      >
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            required
+            onChange={handleChange}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            minLength='6'
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Login'
+          onSubmit={handleSubmit}
+        />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </>
+  );
 };
 
 export default Login;
